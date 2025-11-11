ae37df2f5de5b31a4a03cb8b8fcccb0f58b0a7c0
sofpret <59215603+sofpret@users.noreply.github.com>
2025-11-11 10:03:35 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-green-600 text-white p-4 shadow-md">
+      <h1 className="text-xl font-bold">🚖 Ojek Kontrakan</h1>
+    </nav>
+  );
+}
 
