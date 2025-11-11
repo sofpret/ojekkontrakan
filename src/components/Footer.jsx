119967b03d326693f59276aab13f0b0eac251367
sofpret <59215603+sofpret@users.noreply.github.com>
2025-11-11 10:05:02 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-200 text-center py-3 text-sm mt-4">
+      © 2025 Ojek Kontrakan. All rights reserved.
+    </footer>
+  );
+}
 
