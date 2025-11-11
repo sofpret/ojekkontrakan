26f0faf050c134af7f2bd1a43e277cf55472a187
sofpret <59215603+sofpret@users.noreply.github.com>
2025-11-11 10:07:04 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Ganti ojek-kontrakan sesuai nama repo kamu
+export default defineConfig({
+  plugins: [react()],
+  base: "/ojek-kontrakan/",
+});
 
